0b71cd7a05825a0fb0a54114737258577f2ee0ae
Patrick <markpatrickperdon@gmail.com>
2020-05-10 17:00:17 +0800

    implement uipicker

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -12,14 +12,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var bitcoinLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
+    let coinManager = CoinManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let coinManager = CoinManager()
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
+
         // Do any additional setup after loading the view.
         coinManager.getCoinData()
     }
-
+    
+    
 
 }
 
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+    }
+}
